ab64efeffcdf8a3b49e0ecdf869d1eb8a46e41cc
EidalPRO <eidaloaxaca@gmail.com>
2025-05-19 18:53:50 -0600

    Primer commit: Versión inicial del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+
+  // Regresar arriba boton
+  const backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 300) {
+      backToTopButton.classList.add("visible");
+    } else {
+      backToTopButton.classList.remove("visible");
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
